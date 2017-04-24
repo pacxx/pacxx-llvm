04be264a0f0333bb6e95cda5bac47c4392dcdfe9
Michael Haidl <michael.haidl@uni-muenster.de>
2017-04-24 12:23:31 +0200

    fixed nvvm pass to accept 32 bit modules correctly

--- lib/Transforms/PACXX/nvvm.cpp
+++ lib/Transforms/PACXX/nvvm.cpp
@@ -46,17 +46,22 @@ struct NVVMPass : public ModulePass {
     bool modified = true;
     vector<Function *> deleted_functions;
 
-    unsigned ptrSize = 64; // M.getDataLayout()->getPointerSizeInBits();
-    M.setTargetTriple("nvptx64-unknown-unknown");
+    unsigned ptrSize = M.getDataLayout()->getPointerSizeInBits();
 
-    if (ptrSize == 64)
+
+    if (ptrSize == 64){
+      M.setTargetTriple("nvptx64-unknown-unknown");
+
       M.setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:"
                       "64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-"
                       "v128:128:128-n16:32:64");
-    else
+    }
+    else {
+      M.setTargetTriple("nvptx-unknown-unknown");
       M.setDataLayout("e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:"
-                      "64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-"
-                      "v128:128:128-n16:32:64");
+                          "64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-"
+                          "v128:128:128-n16:32:64");
+    }
 
     auto kernels = pacxx::getTagedFunctions(&M, "nvvm.annotations", "kernel");
 
