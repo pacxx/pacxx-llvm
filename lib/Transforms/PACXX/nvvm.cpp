d2c5252f66ce980cb028730c14eaa4d35a65915f
Michael Haidl <michael.haidl@uni-muenster.de>
2017-08-11 12:17:59 +0200

    fixed data layout for NVPTX backend

--- lib/Transforms/PACXX/nvvm.cpp
+++ lib/Transforms/PACXX/nvvm.cpp
@@ -51,16 +51,12 @@ struct NVVMPass : public ModulePass {
     if (ptrSize == 64){
       M.setTargetTriple("nvptx64-unknown-unknown");
 
-      M.setDataLayout("e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:"
-                      "64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-"
-                      "v128:128:128-n16:32:64");
+      M.setDataLayout("e-i64:64-i128:128-v16:16-v32:32-n16:32:64");
     }
     else {
       M.setTargetTriple("nvptx-unknown-unknown");
 
-      M.setDataLayout("e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:"
-                          "64-f32:32:32-f64:64:64-v16:16:16-v32:32:32-v64:64:64-"
-                          "v128:128:128-n16:32:64");
+      M.setDataLayout("e-i32:32-i128:128-v16:16-v32:32-n16:32:64");
     }
 
     auto replaceSubstring = [](string Str, const StringRef &From,
